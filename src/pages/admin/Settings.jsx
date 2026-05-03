9f56bd33289d8741073c026608e26c8c047f77aa
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-05-03 21:51:35 +0530

    revert padding  in setting page

--- src/pages/admin/Settings.jsx
+++ src/pages/admin/Settings.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 export default function AdminSettings() {
     const { t } = useTranslation(['settings']);
     return (
-        <div className="font-sans min-h-screen p-1 pt-3">
+        <div className="font-sans min-h-screen p-0 pt-3">
             <div className="w-full px-4 py-2">
                 <div className="mb-4">
                     <h3 className="text-xl mb-2 text-gray-900 font-extrabold">{t('settings:title')}</h3>
