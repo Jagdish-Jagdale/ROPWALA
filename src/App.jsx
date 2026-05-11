a5d536f109cf320174d50929f8ebc0686e4d2905
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-05-11 10:02:31 +0530

    remove rr in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   const isOnline = useNetworkStatus();
   const { i18n } = useTranslation();
 
-  // Add lang-mr class for Marathi fontrr
+  // Add lang-mr class for Marathi font
   useEffect(() => {
     if (i18n.language === 'mr') {
       document.body.classList.add('lang-mr');
