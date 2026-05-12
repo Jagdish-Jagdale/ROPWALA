3d6d199fb6438aba8485674ceee1da72e442e446
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-05-12 09:35:32 +0530

    remove jj in ourproduct page

--- src/pages/admin/OurProducts.jsx
+++ src/pages/admin/OurProducts.jsx
@@ -140,7 +140,7 @@ export default function AdminOurProducts() {
 
             setProducts(data);
         } catch (error) {
-            console.error("Error fetching productsjj:", error);
+            console.error("Error fetching products:", error);
             toast.error("Failed to load inventory");
         } finally {
             setLoading(false);
